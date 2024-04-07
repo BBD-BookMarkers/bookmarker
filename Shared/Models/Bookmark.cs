098ac3b7432cb5677d968d10ac3c28ebb3beeabd
Verushan Naidoo <verushan.naidoo@bbd.co.za>
2024-04-07 22:09:44 +0200

    Updated shared models

--- Shared/Models/Bookmark.cs
+++ Shared/Models/Bookmark.cs
@@ -1,11 +1,15 @@
-﻿namespace Shared.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Shared.Models
 {
+    [Table("Bookmark")]
     public class Bookmark
     {
-        public int BookmarkId { get; set; }
+        public int BookmarkId { get; set; } = default;
         public int UserId { get; set; }
-        public int RouteId { get; set; }
         public required string Name { get; set; }
+        [Column("createdDate")]
         public DateTime DateCreated { get; set; }
         public required Route Route { get; set; }
     }
